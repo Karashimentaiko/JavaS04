18af36c52de26f0d2843ad80335c078852ecafe5
石井 友理 <yu-ishii@virtualex.local>
2019-05-08 15:12:20 +0900

    #4経費一覧javaとjs変更-1

--- src/main/java/java_s04/ExpensesResource.java
+++ src/main/java/java_s04/ExpensesResource.java
@@ -1,5 +1,107 @@
 package java_s04;
 
-public class ExpensesResource {
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
+
+import beans.Expenses;
+//import beans.Status;
+import dao.ExpensesDAO;
+
+/**
+ * 経費関連のサービス実装。
+ */
+@Path("expenses")
+public class ExpensesResource {
+	private final ExpensesDAO expDao = new ExpensesDAO();
+
+	/**
+	 * ID指定で経費情報を取得する。
+	 *
+	 * @param id
+	 *            取得対象の経費のID
+	 * @return 取得した経費情報をJSON形式で返す。データが存在しない場合は空のオブジェクトが返る。
+	 */
+	@GET
+	@Path("{id}") // 自分の関数のパス、数字、その都度変えられるようになっている
+	@Produces(MediaType.APPLICATION_JSON)
+	public Expenses findById(@PathParam("id") int id) {
+		return expDao.findById(id);
+	}
+
+	/**
+	 * 指定した経費情報を登録する。
+	 *
+	 * @param form
+	 *            経費情報を収めたオブジェクト
+	 * @return DB上のIDが振られた経費情報
+	 * @throws WebApplicationException
+	 *             入力データチェックに失敗した場合に送出される。
+	 */
+	@POST
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Expenses create(final FormDataMultiPart form) throws WebApplicationException {
+		Expenses expenses = new Expenses();
+
+		expenses.setId(0);
+		expenses.setAppId(form.getField("appId").getValue());
+		expenses.setRepDate(form.getField("repDate").getValue());
+		expenses.setUpDate(form.getField("upDate").getValue());
+		expenses.setName(form.getField("name").getValue());
+		expenses.setTitle(form.getField("title").getValue());
+		expenses.setPayee(form.getField("payee").getValue());
+		expenses.setPrice(Integer.parseInt(form.getField("price").getValue()));
+		String status = form.getField("status").getValue();
+		expenses.setStatus(Status.valueOf(status));
+
+		if (!expenses.isValidObject()) {
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
+
+		return expDao.create(expenses);
+	}
+
+	/**
+	 * 指定した情報でDBを更新する。
+	 *
+	 * @param form
+	 *            更新情報を含めた経費情報
+	 * @throws WebApplicationException
+	 *             入力データチェックに失敗した場合に送出される。
+	 */
+	@PUT
+	@Path("{id}")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Expenses update(@PathParam("id") int id, final FormDataMultiPart form) throws WebApplicationException {
+		Expenses expenses = new Expenses();
+
+		expenses.setId(id);
+		expenses.setAppId(form.getField("appId").getValue());
+		expenses.setRepDate(form.getField("repDate").getValue());
+		expenses.setUpDate(form.getField("upDate").getValue());
+		expenses.setName(form.getField("name").getValue());
+		expenses.setTitle(form.getField("title").getValue());
+		expenses.setPayee(form.getField("payee").getValue());
+		expenses.setPrice(Integer.parseInt(form.getField("price").getValue()));
+		String status = form.getField("status").getValue();
+		expenses.setStatus(Status.valueOf(status));
+
+		if (!expenses.isValidObject()) {
+			throw new WebApplicationException(Response.Status.BAD_REQUEST);
+		}
+
+		return expDao.update(expenses);
+	}
 }
--- src/main/webapp/js/expenses.js
+++ src/main/webapp/js/expenses.js
@@ -1,3 +1,175 @@
-/**
- * 
- */
+'use strict';
+
+var rootUrl = "/java_s04/api/v1.1/expenses";
+
+initPage();
+
+$('#saveExpenses').click(function() {
+	$('.error').children().remove();
+	if ($('#appId').val() === '') {
+		$('.error').append('<div>申請IDは必須入力です。</div>');
+	}
+	if ($('#repDate').val() === '') {
+		$('.error').append('<div>申請日は必須入力です。</div>');
+	}
+	if ($('#name').val() === '') {
+		$('.error').append('<div>申請者は必須入力です。</div>');
+	}
+	if ($('#title').val() === '') {
+		$('.error').append('<div>タイトルは必須入力です。</div>');
+	}
+	if ($('#payee').val() === '') {
+		$('.error').append('<div>支払先は必須入力です。</div>');
+	}
+	if ($('#price').val() === '') {
+		$('.error').append('<div>金額は必須入力です。</div>');
+	}
+	if ($('.error').children().length != 0) {
+		return false;
+	}
+
+	var id = $('#id').val()
+	if (id === '')
+		addExpenses();
+	else
+		updateExpenses(id);
+	return false;
+})
+
+$('#newExpenses').click(function() {
+	renderDetails({});
+});
+
+function initPage() {
+	var newOption = $('<option>').val(0).text('指定しない').prop('selected', true);
+	$('#postIdParam').append(newOption);
+	makePostSelection('#postIdParam');
+	findAll();
+	makePostSelection('#postId');
+}
+
+function findByParam() {
+	console.log('findByParam start.');
+
+	var urlWithParam = rootUrl + '?postId=' + $('#postIdParam').val()
+			+ '&empId=' + $('#empIdParam').val() + '&nameParam='
+			+ $('#nameParam').val();
+	$.ajax({
+		type : "GET",
+		url : urlWithParam,
+		dataType : "json",
+		success : renderTable
+	});
+}
+
+function addExpenses() {
+	console.log('addExpenses start');
+
+	var fd = new FormData(document.getElementById("expensesForm"));
+
+	$.ajax({
+		url : rootUrl,
+		type : "POST",
+		data : fd,
+		contentType : false,
+		processData : false,
+		dataType : "json",
+		success : function(data, textStatus, jqXHR) {
+			alert('経費の追加に成功しました');
+			findAll();
+			renderDetails(data);
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('経費の追加に失敗しました');
+		}
+	})
+}
+
+function updateExpenses(id) {
+	console.log('updateExpenses start');
+
+	var fd = new FormData(document.getElementById("expensesForm"));
+
+	$.ajax({
+		url : rootUrl + '/' + id,
+		type : "PUT",
+		data : fd,
+		contentType : false,
+		processData : false,
+		dataType : "json",
+		success : function(data, textStatus, jqXHR) {
+			alert('経費の更新に成功しました');
+			findAll();
+			renderDetails(data);
+		},
+		error : function(jqXHR, textStatus, errorThrown) {
+			alert('経費の更新に失敗しました');
+		}
+	})
+}
+
+function renderTable(data) {
+	var headerRow = '<tr><th>申請ID</th><th>申請日</th><th>更新日</th><th>申請者</th><th>タイトル</th><th>金額</th><th>ステータス</th>';
+
+	$('#expenses').children().remove();
+
+	if (data.length === 0) {
+		$('#expenses').append('<p>現在データが存在していません。</p>')
+	} else {
+		var table = $('<table>').attr('border', 1);
+		table.append(headerRow);
+
+		$.each(data, function(index, expenses) {
+			var row = $('<tr>');
+			row.append($('<td>').text(expenses.appId));
+			row.append($('<td>').text(expenses.repDate);
+			row.append($('<td>').text(expenses.upDate);
+			row.append($('<td>').text(expenses.name));
+			row.append($('<td>').text(expenses.title));
+			row.append($('<td>').text(expenses.price));
+			row.append($('<td>').text(expenses.status);
+			row.append($('<td>').append(
+					$('<button>').text("編集").attr("type","button").attr("onclick", "findById("+expenses.id+')')
+				));
+			row.append($('<td>').append(
+					$('<button>').text("詳細").attr("type","button").attr("onclick", "findById("+expenses.id+')')
+				));
+			table.append(row);
+		});
+
+		$('#expenses').append(table);
+	}
+}
+
+function renderDetails(expenses) {
+	$('.error').text('');
+	$('#id').val(expenses.id);
+	$('#appId').val(expenses.appId);
+	$('#repDate').val(expenses.repDate);
+	$('#upDate').val(expenses.upDate);
+	$('#name').val(expenses.name);
+	$('#title').val(expenses.title);
+	$('#payee').val(expenses.payee);
+	$('#price').val(expenses.price);
+	$('input[name="status"]').val([ expenses.status ]);
+	$('#upName').val(expenses.upName);
+	$('#rejectReason').val(expenses.rejectReason);
+}
+
+function makePostSelection(selectionId, expenses) {
+	console.log('makePostSelection start.')
+	$.ajax({
+		type : "GET",
+		url : getPostsUrl,
+		dataType : "json",
+		success : function(data, textStatus, jqXHR) {
+			$.each(data, function(index, post) {
+				var newOption = $('<option>').val(post.id).text(post.name);
+				if (expenses != null && expenses.post.id == post.id) {
+					newOption.prop('selected', isSelected);
+				}
+				$(selectionId).append(newOption);
+			});
+		}
+	});
+}
